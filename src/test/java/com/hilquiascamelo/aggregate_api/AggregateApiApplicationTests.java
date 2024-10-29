0904ca4b9199a8f0d768e06ec2c047716c1437f6
HilquiasCamelo <hilquias@ciandt.com>
2024-10-29 09:31:33 -0300

    chore: First commit

---
+++ src/test/java/com/hilquiascamelo/aggregate_api/AggregateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilquiascamelo.aggregate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
